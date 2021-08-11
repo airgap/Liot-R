3ffce8da600c528510521aad4e91a5d7bbbba3a9
Nicole Muzzin <nick.muzzin@gmail.com>
2021-08-10 21:50:18 -0400

    Fixed typo

--- server/modules/insert-distributors.js
+++ server/modules/insert-distributors.js
@@ -3,9 +3,8 @@
  * Inserts one or more packet distributors.
  * @name Database: Insert Distributors
  * @function
- * @param {object} CONNECTION - Connection to the RethinkDB database
+ * @param {object} r - Connection to the RethinkDB database
  * @param {array} distributors - List of distributors to insert
- * @param {function} callback - Function(error, result) to execute upon success or error
  */
 export const insertDistributors = (r, distributors) =>
   r.table('Distributors').insert(distributors, { conflict: 'replace' });
