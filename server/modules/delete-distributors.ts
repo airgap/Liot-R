863e81d513f170660b35838674454e402edacabd
Nicole Muzzin <nick.muzzin@gmail.com>
2021-08-10 21:20:03 -0400

    Modernized

---
+++ server/modules/delete-distributors.ts
@@ -0,0 +1,11 @@
+/**
+ * Delete one or more packet distributors.
+ * @name Database: Delete Distributors
+ * @function
+ * @param {object} r - Connection to the RethinkDB database
+ * @param {array} distributors - List of distributor IDs to delete
+ */
+export const deleteDistributors = (r, distributors) =>
+  r.table('Distributors')
+    .filter(doc=>{return r.expr(distributors).contains(doc('id'))})
+      .delete();
---
+++ server/modules/get-collators.ts
@@ -0,0 +1,15 @@
+/**
+ * Retreive one or more filter collators.
+ * @name Database: Get Collators
+ * @function
+ * @param {object} r - Connection to the RethinkDB database
+ * @param {array} collators - List of collators to retrieve
+ */
+export const getCollators = (r, collators) =>
+    r.table('Collators')
+        .filter(doc => r.expr(collators).contains(doc('id')))
+        .merge(doc => ({
+            filtrets: r.table('Filters')
+                .getAll(r.args(doc('filters'))).coerceTo('array')
+        }))
+        .coerceTo('array');
