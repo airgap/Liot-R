c22294187aee19a66e514d094d0c8be53da2c22e
Nicole Muzzin <nick.muzzin@gmail.com>
2021-08-10 22:04:07 -0400

    Updated JSDoc

--- server/modules/insert-collators.ts
+++ server/modules/insert-collators.ts
@@ -2,9 +2,8 @@
  * Inserts one or more filter collators.
  * @name Database: Insert Collators
  * @function
- * @param {object} CONNECTION - Connection to the RethinkDB database
+ * @param {object} r - Connection to the RethinkDB database
  * @param {array} collators - List of collators to insert
- * @param {function} callback - Function(error, result) to execute upon success or error
  */
 export const insertCollators = (r, collators)  =>
   r.table('Collators').insert(collators, { conflict: 'replace' })
