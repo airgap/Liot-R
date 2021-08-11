3c2347a7d57d383fd8780e40429372ea3b5bbe09
Nicole Muzzin <nick.muzzin@gmail.com>
2021-08-10 22:53:31 -0400

    Modernized

--- server/modules/actions/action-get-filters.ts
+++ server/modules/actions/action-get-filters.ts
@@ -1,28 +1,22 @@
-var getFilters = require('../get-filters')
+import {getFilters} from "../get-filters";
+
 /**
  * Retreive the contents of one or more packet filters.
  * @name Action: Get Filters
  * @function
- * @param {boolean} DEBUG - enable verbose logging
- * @param {object} CONNECTION - connection to the RethinkDB database
- * @param {object} req - Express request
- * @param {object} res - Express response
+ * @param {object} params - request params supposedly containing a list of filter IDs
+ * @param {object} r - connection to the RethinkDB database
  */
-function actionGetFilters (DEBUG, CONNECTION, req, res) {
-  if(!Array.isArray(req.body.ids)) {
-    res.send({err: "No list of IDs provided."});
-    return;
-  }
-  for(var id of req.body.ids)if(typeof id != 'string' || id.length > 55) { res.send({err: 'Invalid (non-string) ID provided.'}); return }
-  getFilters(CONNECTION, req.body.ids, (err, collators) => {
-    if(err) {
-      res.send({err: 'Unable to getfilters.'});
-      if(DEBUG)console.log(err);
-    } else {
-      res.send({filters:collators});
-      if(DEBUG)console.log('Got filters.');
-      if(DEBUG)console.log(collators);
+async function actionGetFilters({ids}, r) {
+    if (!Array.isArray(ids))
+        return {err: "No list of IDs provided."};
+    for (const id of ids)
+        if (typeof id != 'string' || id.length > 55)
+            return {err: 'Invalid (non-string) ID provided.'};
+    try {
+        const filters = await getFilters(r, ids);
+        return {filters};
+    } catch (err) {
+        return {err: 'Unable to get filters.'};
     }
-  })
 }
-module.exports = actionGetFilters
