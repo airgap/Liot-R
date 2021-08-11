5cb63932f74fbf7dacf9ef5393906a94cf2caef3
Nicole Muzzin <nick.muzzin@gmail.com>
2021-08-10 22:06:01 -0400

    Modernized

--- server/modules/list-filters.js
+++ server/modules/list-filters.js
@@ -1,28 +1,18 @@
-var r = require('rethinkdb')
 /**
  * List nonspecific packet filters.
  * @name Database: List Collators
  * @function
- * @param {object} CONNECTION - Connection to the RethinkDB database
+ * @param {object} r - Connection to the RethinkDB database
  * @param {number} after - skip this many filters in query
  * @param {number} count - number of filters to return
  * @param {string|object} order - key or method for sorting
- * @param {function} callback - Function(error, result) to execute upon success or error
  */
-function listFilters(CONNECTION, after, count, order, callback) {
-  r.table('Filters').count().run(CONNECTION, (err, total) => {
-    if(err) {
-
-    } else {
-      if(after<0)after += total;
-      after = Math.max(0,after);
-      var query = r.table('Filters')
+export const listFilters = async (r, after, count, order) => {
+    const total = await r.table('Filters').count();
+    if (after < 0) after += total;
+    after = Math.max(0, after);
+    return await r.table('Filters')
         .orderBy(order)
-          .slice(after, after + count)
-            .coerceTo('array')
-              .run(CONNECTION, callback)
-    }
-
-  })
+        .slice(after, after + count)
+        .coerceTo('array')
 }
-module.exports = listFilters
