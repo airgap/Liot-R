c5c1318e4dbddad640d7c04426c024a9555ea5db
Nicole Muzzin <nick.muzzin@gmail.com>
2021-08-10 21:58:54 -0400

    Modernized

---
+++ server/modules/list-collators.ts
@@ -0,0 +1,21 @@
+/**
+ * Lists filter collators.
+ * @name Database: List Collators
+ * @function
+ * @param {object} r - Connection to the RethinkDB database
+ * @param {number} after - skip this many collators in query
+ * @param {number} count - number of collators to return
+ * @param {string|object} order - key or method for sorting
+ */
+
+export const listCollators = async (r, after, count, order) => {
+    const total = await r.table('Collators').count();
+    if (after < 0)
+        after += total;
+    after = Math.max(0, after);
+    return r.table('Collators')
+        .orderBy(order)
+        .slice(after, after + count).merge(doc => ({
+            filtrets: r.table('Filters').getAll(r.args(doc('filters'))).coerceTo('array')
+        }));
+}
