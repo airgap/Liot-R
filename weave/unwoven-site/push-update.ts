587edb8cb73bd3898be8eb069b129f0906cc71da
Nicole Muzzin <nick.muzzin@gmail.com>
2021-08-15 04:02:05 -0400

    Split JS out into TS

---
+++ weave/unwoven-site/push-update.ts
@@ -0,0 +1,34 @@
+import { grab, setc } from './bonus.js';
+import { LiotR } from './liotr.js';
+
+const liotR = new LiotR();
+
+function save() {
+	//addc('save-button', 'disabled');
+	err('Creating distributor...');
+	var update = grab('update').value;
+
+	liotR.pushUpdate(JSON.parse(update), res => {
+		if (res.err) {
+			console.log(res.err);
+			return;
+		}
+		location.href = '/distributors';
+	});
+
+	//Liotr.pushUpdate({value:98,accessor:'<AUTH_CODE>'});
+}
+
+function err(text) {
+	grab('compile-errors').innerHTML = text || 'Ready';
+	setc('compile-errors', 'invalid-json', text);
+	setc('save-button', 'disabled', text);
+}
+
+function xss(text) {
+	return text.replace('<', '&lt');
+}
+
+function nelem(type) {
+	return document.createElement(type);
+}
