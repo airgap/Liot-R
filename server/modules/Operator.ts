54df2db497fd824e8f5e72736f181db34c63c8c5
Nicole Muzzin <nick.muzzin@gmail.com>
2021-08-15 06:19:56 -0400

    Added isOperator

--- server/modules/Operator.ts
+++ server/modules/Operator.ts
@@ -1,6 +1,9 @@
+import * as operators from './operators/operators';
 export abstract class Operator {
 	static match: RegExp = /^$/;
 	static minInputs: number = 2;
 	static maxInputs: number = 2;
 	static evaluate: (operands: unknown[]) => unknown = () => null;
+	static isOperator = (text: string) =>
+		Object.values(operators).find(op => op.match.test(text));
 }
